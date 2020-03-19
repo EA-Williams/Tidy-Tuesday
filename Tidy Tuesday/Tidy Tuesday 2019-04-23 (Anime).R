50bf5e3d7bb804aa7a7484575de43fdec3663db6
Emily A. Williams <e.a.williams@leeds.ac.uk>
2020-03-19 18:39:02 +0000

    Update Tidy Tuesday 2019-04-23 (Anime).R

--- Tidy Tuesday/Tidy Tuesday 2019-04-23 (Anime).R
+++ Tidy Tuesday/Tidy Tuesday 2019-04-23 (Anime).R
@@ -29,25 +29,55 @@ anime <- tidy_anime %>%
   
   # Q1: how does broadcast day / time affect the score of a series?
   
+  # function to convert "broadcast" to numbered day ('bcd' is broadcast day)
+  
+ bcd_to_num <- function(bcd) {
+    day_num <- str_extract(bcd, '[A-Za-z]+') %>%
+      recode("Mondays" = 1,
+             "Tuesdays" = 2,
+             "Wednesdays" = 3,
+             "Thursdays" = 4,
+             "Fridays" = 5,
+             "Saturdays" = 6,
+             "Sundays" = 7)
+    return(day_num)
+  }
+  
   broadcasting <- raw_df %>%
     select(animeID, name, score, broadcast) %>%
-    mutate(bc_day = str_extract(broadcast, '[A-Za-z]+'),
-           bc_day = substr(bc_day, 1, nchar(bc_day)-1),
+    mutate(bc_day_num = bcd_to_num(broadcast),
            bc_time = str_extract(broadcast, '[:digit:][:digit:][:punct:][:digit:][:digit:]'),
            bc_hour = hour(strptime(bc_time, format = "%H:%M"))) %>%
-    filter(!is.na(bc_day), !is.na(bc_hour))
+    filter(!is.na(bc_day_num), !is.na(bc_hour))
     
-  ggplot(broadcasting, aes(x = bc_day, y = bc_hour, fill = score)) + 
+  ggplot(broadcasting, aes(x = bc_day_num, y = bc_hour, fill = score)) + 
     geom_tile(colour="black", size=0.5, stat="identity") + 
-    scale_fill_viridis(name = "Anime Score") #+
-   # scale_x_discrete(breaks=1:7, labels= c("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"), position= "top", expand = c(0,0))# +
-    #scale_y_discrete(breaks = seq(1960, 2010, by = 10)) +
-    labs(title="UFO Sightings since 1960 by Month and Year",
-         x ="Month", y = "Year") + tile_theme
-  day("Monday")
+    scale_fill_viridis(name = "Anime Score") +
+    scale_x_continuous(breaks=1:7, labels= c("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"), position= "top",
+                                             expand = c(0,0)) +
+    scale_y_continuous(breaks = seq(0, 23, by = 2), expand = c(0,0)) +
+    tile_theme +
+    labs(title="Anime Scores by Broadcast Day and Hour",
+         x ="Broadcast Day", y = "Broadcast Hour") + tile_theme
+    
+  # Hmm, quite few blank tiles - dark hours where no anime is aired!
   
-  df$wkdaynum <- format("Monday","%u")  
+  # Let's simplify it and look at just days of the week...
   
+  # Q1b: how does broadcast day effect the score of a series?
+  
+  broadcasting2 <- broadcasting %>%
+    group_by(bc_day_num) %>%
+    mutate(n_day = n())
+  
+  # plot the days on the x axis and scores on the y axis
+  ggplot(broadcasting2, aes(x = bc_day_num, y = score, fill = n_day)) +
+    # add the columns for each day, use a minimal theme and the viridis colour palette (the best)
+    geom_col() + scale_fill_viridis() + theme_minimal()
+    # label the plot and the axes
+    labs(title="Scores of Animes by Broadcast Day",
+         x ="Broadcast Day", y = "Score")
+ 
       
       # Q3: 
       # 1.	Dragon Ball Z
